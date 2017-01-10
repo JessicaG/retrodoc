85a19a306d92e726b4a51198f42172b4e0bcce5b
Solomon White <rubysolo@gmail.com>
2017-01-09 18:49:06 -0700

    get tests passing

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,12 +3,12 @@ defmodule Retrodoc.UserTest do
 
   alias Retrodoc.User
 
-  @valid_attrs %{crypted_password: "some content", email: "some content"}
+  @valid_attrs %{password: "some content", email: "user@domain.tld"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
     changeset = User.changeset(%User{}, @valid_attrs)
-    assert changeset.valid?
+    assert changeset.valid?, changeset.errors
   end
 
   test "changeset with invalid attributes" do
--- web/controllers/registration_controller.ex
+++ web/controllers/registration_controller.ex
@@ -13,16 +13,16 @@ defmodule Retrodoc.RegistrationController do
     case Retrodoc.Registration.create(changeset, Retrodoc.Repo) do
       # sign in the user
       {:ok, _user} ->
-      conn
-      |> put_session(:current_user, changeset.id)
-      |> put_flash(:info, "Your account was created")
-      #|> redirect(to: page_path(conn, :index))
-      |> redirect(to: "/")
+        conn
+        |> put_session(:current_user, changeset.id)
+        |> put_flash(:info, "Your account was created")
+        #|> redirect(to: page_path(conn, :index))
+        |> redirect(to: "/")
 
       # show error message
       {:error, changeset} ->
-      #|> put_flash(:info, "Unable to register user")
-      render(conn, "new.html", changeset: changeset)
+        #|> put_flash(:info, "Unable to register user")
+        render(conn, "new.html", changeset: changeset)
     end
   end
 
